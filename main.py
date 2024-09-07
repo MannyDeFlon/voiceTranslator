866e52f193fbb293f628111c57309c02e6aec7c6
Manny <7494822+ZapoColorado@users.noreply.github.com>
2024-09-07 13:05:59 -0600

    first commit

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ elevens_api_key = os.getenv("ELEVENLABS_API_KEY")
 save_file_path = "audio.mp3"
 
 
-def voice_translator(audio_file):
+def voice_translator(audio_file : str, to_lang : str = "en") -> str:
+    """
+    Translate voice from different lenguages in real time.
+
+    audio_file: path to the audio file
+    to_lang: language to translate to
+
+    returns: path to the saved file
+    """
 
     # transcribir audio
     try:
@@ -25,30 +33,43 @@ def voice_translator(audio_file):
     # traducir texto
     try:
         en_translation = Translator(from_lang="es", to_lang="en").translate(transcription)
+        fr_translation = Translator(from_lang="es", to_lang="fr").translate(transcription)
+
 
     except Exception as e:
         gr.Error(f"Hubo un error al traducir el texto: {str(e)}")
 
     # generar audio
     try: 
-        client = ElevenLabs(api_key="sk_36653306404c3d97c5107a850325db9d2b6fb5675a91ca37")
-        print("Apoco si mailove")
-        response = client.text_to_speech.convert(text=en_translation,
-                                              voice_id="pNInz6obpgDQGcFmaJgB",  # Adam
-                                              optimize_streaming_latency="0",
-                                              output_format="mp3_22050_32",
-                                              model_id="eleven_multilingual_v2")
-        
-        with open(save_file_path, "wb") as f:
-            for chunk in response:
-                if chunk:
-                    f.write(chunk)
+        text_to_speech(en_translation, language="en")
 
     except Exception as e:
         gr.Error(f"Hubo un error al generar el audio: {str(e)}")
         print(f"Hubo un error al generar el audio: {str(e)}")
 
     return save_file_path  
+
+def text_to_speech(text: str, language: str = "en") -> str:
+    """
+    Convert text to speech and save it to a file. Returns path to the saved file.
+    """
+
+    client = ElevenLabs()
+
+    response = client.text_to_speech.convert(text=text,
+                                            voice_id="pNInz6obpgDQGcFmaJgB",  # Adam
+                                            optimize_streaming_latency="0",
+                                            output_format="mp3_22050_32",
+                                            model_id="eleven_multilingual_v2")
+
+    save_file_path = f"audio_{language}.mp3"
+
+    with open(save_file_path, "wb") as f:
+            for chunk in response:
+                if chunk:
+                    f.write(chunk)
+
+    return save_file_path
 
 
 web = gr.Interface(
